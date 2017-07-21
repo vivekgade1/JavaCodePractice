ea3494a2ab053b4f36f259f2a6bb163d58aaa69c
Vivek Gade <vgade@vmx-mm-1681.verimatrix.com>
2017-07-21 16:17:36 -0700

    string prblem 2

--- src/interviewBit/Numbers/StringProblems.java
+++ src/interviewBit/Numbers/StringProblems.java
@@ -5,7 +5,7 @@ package interviewBit.Numbers;
  */
 public class StringProblems {
     public static void main(String[] args){
-        System.out.println(isPalindrome(null));
+        System.out.println(checkPositionFromEnd("",'k'));
 
     }
     /*----------palindrom string-------*/
@@ -34,8 +34,10 @@ public class StringProblems {
 
         }catch (NullPointerException e){
             System.out.println("Got a null value");
+            return 0;
         } catch(StringIndexOutOfBoundsException e) {
             System.out.println("check for low and high values");
+            return 0;
 
         }
         return is_palindrome;
@@ -44,6 +46,46 @@ public class StringProblems {
     /*----------end palindrom string-------*/
 
     /*-------------------------- minimum characters to make it a palindrome --------------------------*/
+    public int solve(String A){
+            try {
+                int size = A.length();
+                int end = size-1;
+                int last_occurrance;
+                StringBuffer sub_string_a = new StringBuffer(A);
+                while(isPalindrome(sub_string_a.toString()) == 0){
+                    last_occurrance = checkPositionFromEnd(sub_string_a.toString(),A.charAt(end));
+                    sub_string_a = (StringBuffer) sub_string_a.subSequence(0,last_occurrance+1);
+                }
+
+            }catch (NullPointerException e){
+
+            }
+
+            return 0;
+
+    }
+
+    public static int checkPositionFromEnd(String s, char n){
+        try{
+            int size = s.length();
+            int high = size -1;
+            while (high != -1){
+                if(s.charAt(high) != n){
+                    high --;
+                }else{
+                    return high;
+                }
+            }
+            return high;
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            return -1;
+        }catch (StringIndexOutOfBoundsException e){
+            System.out.println("check low and high indexes");
+            return -1;
+        }
+
+    }
 
     
     /*-------------------------- end minimum characters to make it a palindrome --------------------------*/
